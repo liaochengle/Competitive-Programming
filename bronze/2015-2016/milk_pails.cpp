2932ed07b93113434d014d60afe12314f78dfc4b
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-01-16 07:50:22 +0800

    Update milk_pails.cpp

--- bronze/2015-2016/milk_pails.cpp
+++ bronze/2015-2016/milk_pails.cpp
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
 
+int milk_pails()
+{
+	freopen("pails.in", "r", stdin);
+	freopen("pails.out", "w", stdout);
+
+	int x, y, m;
+	scanf("%d %d %d", &x, &y, &m);
+	int xn = int(m / x) + 1;
+	int yn = int(m / y) + 1;
+	int ans = 0;
+	for (int i = 0; i < xn; i++)
+	{
+		for (int j = 0; j < yn; j++)
+		{
+			int temp = x * i + y * j;
+			if (temp > m)
+				continue;
+			ans = max(ans, temp);
+		}
+	}
+	printf("%d", ans);
+	return 0;
+}
