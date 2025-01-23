8703b8b9a00b4e54db5b4dd41a93160b3727ee65
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-01-23 17:25:47 +0800

    Update square_pasture.cpp

--- USACO solutions/Bronze/2016-2017/square_pasture.cpp
+++ USACO solutions/Bronze/2016-2017/square_pasture.cpp
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
 
+struct point
+{
+	int x1, y1, x2, y2;
+};
+
+int square_pasture()
+{
+	freopen("square.in", "r", stdin);
+	freopen("square.out", "w", stdout);
+
+	point points[2];
+	for (int i = 0; i < 2; i++)
+		scanf("%d %d %d %d", &points[i].x1, &points[i].y1, &points[i].x2, &points[i].y2);
+
+	int x = max(points[0].x2, points[1].x2) - min(points[0].x1, points[1].x1);
+	int y = max(points[0].y2, points[1].y2) - min(points[0].y1, points[1].y1);
+	int side = max(x, y);
+	printf("%d", side * side);
+	return 0;
+}
