a494f58bb29a07e6eaf4633bfa60bdd46785a2a6
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-01-24 19:10:03 +0800

    Update why_did_the_cow_cross_the_road.cpp

--- USACO solutions/Bronze/2016-2017/why_did_the_cow_cross_the_road.cpp
+++ USACO solutions/Bronze/2016-2017/why_did_the_cow_cross_the_road.cpp
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
 
+int cross_the_road()
+{
+	freopen("crossroad.in", "r", stdin);
+	freopen("crossroad.out", "w", stdout);
+
+	int n;
+	scanf("%d", &n);
+	vector<pair<int, int>> records(n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d %d", &records[i].first, &records[i].second);
+	}
+	int ans = 0;
+	vector<int> cows(11, -1);
+	for (int i = 0; i < n; i++)
+	{
+		int index = records[i].first;
+		int pos = records[i].second;
+		if (cows[index] == -1)
+		{
+			cows[index] = pos;
+			continue;
+		}
+		if (cows[index] != pos)
+		{
+			ans += 1;
+			cows[index] = pos;
+		}
+	}
+	printf("%d", ans);
+	return 0;
+}
