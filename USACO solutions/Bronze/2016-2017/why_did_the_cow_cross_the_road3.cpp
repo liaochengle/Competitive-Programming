8ec4289ddcf2988e095b543dbc339f206658581c
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-01-25 00:07:52 +0800

    Update why_did_the_cow_cross_the_road3.cpp

--- USACO solutions/Bronze/2016-2017/why_did_the_cow_cross_the_road3.cpp
+++ USACO solutions/Bronze/2016-2017/why_did_the_cow_cross_the_road3.cpp
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+struct cow
+{
+	int t, d;
+};
+
+bool cmp(cow a, cow b)
+{
+	if (a.t == b.t)
+		return a.d < b.d;
+	return a.t < b.t;
+}
+
+int cross_road3()
+{
+	freopen("cowqueue.in", "r", stdin);
+	freopen("cowqueue.out", "w", stdout);
+
+	int n;
+	scanf("%d", &n);
+	vector<cow> cows(n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d %d", &cows[i].t, &cows[i].d);
+	}
+	sort(cows.begin(), cows.end(), cmp);
+	int ans = cows[0].t;
+	for (int i = 0; i < n; i++)
+	{
+		ans += cows[i].d;
+		if (i < (n - 1))
+		{
+			if (ans < cows[i + 1].t)
+				ans = cows[i + 1].t;
+		}
+	}
+	printf("%d", ans);
+	return 0;
+}
