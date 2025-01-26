74b81169de0a3c1607d48a85664b127a4fd06ed9
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-01-26 16:28:35 +0800

    Update the_last_cow.cpp

--- USACO solutions/Bronze/2016-2017/the_last_cow.cpp
+++ USACO solutions/Bronze/2016-2017/the_last_cow.cpp
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
 
+int the_lost_cow()
+{
+	freopen("lostcow.in", "r", stdin);
+	freopen("lostcow.out", "w", stdout);
+
+	int x, y;
+	scanf("%d %d", &x, &y);
+	int index = x;
+	int dis = 1;
+	int ans = 0;
+
+	while (index != y)
+	{
+		if (dis > 0)
+		{
+			for (index; index < x + dis; index++)
+			{
+				if (index == y)
+				{
+					printf("%d", ans);
+					return 0;
+				}
+				ans += 1;
+			}
+			dis *= -2;
+		}
+		if (dis < 0)
+		{
+			for (index; index > x + dis; index--)
+			{
+				if (index == y)
+				{
+					printf("%d", ans);
+					return 0;
+				}
+				ans += 1;
+			}
+			dis *= -2;
+		}
+	}
+	printf("%d", ans);
+	return 0;
+}
