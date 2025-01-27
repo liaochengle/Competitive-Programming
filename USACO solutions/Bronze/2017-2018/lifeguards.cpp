e35758d94ae2a0c7eaf7360c2d4034ca161de12c
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-01-27 16:13:03 +0800

    Update lifeguards.cpp

--- USACO solutions/Bronze/2017-2018/lifeguards.cpp
+++ USACO solutions/Bronze/2017-2018/lifeguards.cpp
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
 
+int lifeguards()
+{
+	freopen("lifeguards.in", "r", stdin);
+	freopen("lifeguards.out", "w", stdout);
+
+	int n;
+	scanf("%d", &n);
+	vector<int> start(n);
+	vector<int> end(n);
+	for (int i = 0; i < n; i++)
+		scanf("%d %d", &start[i], &end[i]);
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		vector<int> time(1001, 0);
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j)
+				continue;
+			int st = start[j];
+			int et = end[j];
+			for (int k = st; k < et; k++)
+				time[k] = 1;
+		}
+		int temp = count(time.begin(), time.end(), 1);
+		ans = max(ans, temp);
+	}
+	printf("%d", ans);
+	return 0;
+}
