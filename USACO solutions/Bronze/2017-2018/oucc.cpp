c1967e6fdb22de59e99b528141595ff28a156dd2
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-01-28 15:21:22 +0800

    Update oucc.cpp

--- USACO solutions/Bronze/2017-2018/oucc.cpp
+++ USACO solutions/Bronze/2017-2018/oucc.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int oucc()
+{
+	int n;
+	cin >> n;
+	int stat[10];
+	for (int i = 0; i < n; i++)
+		cin >> stat[i];
+	int ans = 0;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (stat[i + 1] > stat[i])
+			ans += 1;
+	}
+	cout << ans * 100;
+	return 0;
+}
