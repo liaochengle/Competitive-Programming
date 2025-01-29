c9c0176186b3ac57001e6aefb32c5a9c9cc9c28d
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-01-29 10:46:24 +0800

    Update teleportation.cpp

--- USACO solutions/Bronze/2017-2018/teleportation.cpp
+++ USACO solutions/Bronze/2017-2018/teleportation.cpp
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int teleportation()
+{
+	freopen("teleport.in", "r", stdin);
+	freopen("teleport.out", "w", stdout);
+
+	int a, b, x, y;
+	scanf("%d %d", &a, &b);
+	scanf("%d %d", &x, &y);
+	if (a >= b)
+	{
+		int temp;
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	if (x>= y)
+	{
+		int temp;
+		temp = x;
+		x = y;
+		y = temp;
+	}
+	int ans = b - a;
+	int temp = abs(a - x) + abs(b - y);
+	ans = min(ans, temp);
+	printf("%d", ans);
+	return 0;
+}
 
