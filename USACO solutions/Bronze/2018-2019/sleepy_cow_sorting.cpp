d3a6e90e84a6daa9abd713f454b8f587105a3f4c
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-02-01 15:18:14 +0800

    Update sleepy_cow_sorting.cpp

--- USACO solutions/Bronze/2018-2019/sleepy_cow_sorting.cpp
+++ USACO solutions/Bronze/2018-2019/sleepy_cow_sorting.cpp
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
 
+int sleepy_cow_sorting()
+{
+	freopen("sleepy.in", "r", stdin);
+	freopen("sleepy.out", "w", stdout);
+
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	int ans = 0;
+	for (int i = n - 2; i >= 0; i--)
+	{
+		if (arr[i] > arr[i + 1])
+		{
+			ans = i + 1;
+			break;
+		}
+	}
+	cout << ans;
+	return 0;
+}
