62d28273d62a6869741215a7d09f329acf8945f7
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-02-06 20:19:02 +0800

    Update where_am_i.cpp

--- USACO solutions/Bronze/2019-2020/where_am_i.cpp
+++ USACO solutions/Bronze/2019-2020/where_am_i.cpp
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+bool no_same_subset(int length, string& mailbox, int k)
+{
+	set<string> unique;
+	vector<string> subset;
+	for (int i = 0; i < k - length + 1; i++)
+	{
+		string t = mailbox.substr(i, length);
+		subset.push_back(t);
+		unique.insert(t);
+	}
+	if (subset.size() != unique.size())
+		return false;
+	return true;
+}
+
+int where_am_i()
+{
+	freopen("whereami.in", "r", stdin);
+	freopen("whereami.out", "w", stdout);
+
+	int k;
+	cin >> k;
+	string mailbox;
+	cin >> mailbox;
+	int ans = 0;
+	for (int i = 1; i <= k; i++)
+	{
+		vector<string> subset;
+		if (no_same_subset(i, mailbox, k))
+		{
+			cout << i;
+			return 0;
+		}
+	}
+	return 0;
+}
 
