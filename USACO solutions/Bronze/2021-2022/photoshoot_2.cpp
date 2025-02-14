dcdaf3d4e35666d7bb6fc61a9380ff87011d664e
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-02-14 20:04:52 +0800

    Update photoshoot_2.cpp

--- USACO solutions/Bronze/2021-2022/photoshoot_2.cpp
+++ USACO solutions/Bronze/2021-2022/photoshoot_2.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+void in_order(vector<int>& ori, vector<int>& arr, vector<int>& ans, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			int i_ind = find(arr.begin(), arr.end(), ori[i]) - arr.begin();
+			int j_ind = find(arr.begin(), arr.end(), ori[j]) - arr.begin();
+			//cout << i << " " << j << " " << i_ind << " " << j_ind << endl;
+			if (i_ind > j_ind)
+				ans[j] = 1;
+		}
+	}
+}
+
+int photoshoot_2()
+{
+	int n;
+	cin >> n;
+	vector<int> ori(n);
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+		cin >> ori[i];
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	vector<int> ans(n, 0);
+	in_order(ori, arr, ans, n);
+	int answer = 0;
+	for (int i = 0; i < n; i++)
+	{
+		answer += ans[i];
+	}
+	cout << answer << endl;
+	return 0;
+}
 
