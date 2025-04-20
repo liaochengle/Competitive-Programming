c6caea17d3aa6ee984422ec466c45801d7a9ab1f
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-04-20 22:45:50 +0800

    Update back_and_forth.cpp

--- USACO solutions/Bronze/2018-2019/back_and_forth.cpp
+++ USACO solutions/Bronze/2018-2019/back_and_forth.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <set>
 #include <algorithm>
-#include <b
+#include <bi
 using namespace std;
 void even(set<int>& f, vector<int>& a, vector<int>& b, int c, int d, int e);
 
