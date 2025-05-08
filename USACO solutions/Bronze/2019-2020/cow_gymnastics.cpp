c69e49c048817e13ffd8b2c901b8900935c4d4db
liaochengle <98654151+liaochengle@users.noreply.github.com>
2025-05-08 20:07:58 +0800

    Update cow_gymnastics.cpp

--- USACO solutions/Bronze/2019-2020/cow_gymnastics.cpp
+++ USACO solutions/Bronze/2019-2020/cow_gymnastics.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <bi
+#include <bit
 using namespace std;
 
 bool is_consistent(int a, int b, vector<vector<int>>& stat, int k, int n)
